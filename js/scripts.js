d8320b76fd766bf51bfde42bf092f9575bc9074c
Esteban de la Maza <edelamaza@me.com>
2020-07-06 19:38:10 -0500

    added horizontal scroll button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,61 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function() {
+    $('.js--section-images').waypoint(function(direction) {
+        if (direction =="down") {
+            $('.header').addClass('header__sticky')
+        }else {
+            $('.header').removeClass('header__sticky')
+
+        }
+    }, {
+        offset:'120%;'
+    });
+
+});
+
+$('.right-button').click(function() {
+    event.preventDefault();
+    $('.js--section-best-sellers__items').animate({
+      scrollLeft: "+=200px"
+    }, "slow");
+ });
+ 
+   $('.left-button').click(function() {
+    event.preventDefault();
+    $('.js--section-best-sellers__items').animate({
+      scrollLeft: "-=200px"
+    }, "slow");
+ });
+
+
+ $('.right-button--2').click(function() {
+    event.preventDefault();
+    $('.js--section-the-court__right--bottom').animate({
+      scrollLeft: "+=300px"
+    }, "slow");
+ });
+ 
+   $('.left-button--2').click(function() {
+    event.preventDefault();
+    $('.js--section-the-court__right--bottom').animate({
+      scrollLeft: "-=300px"
+    }, "slow");
+ }); 
+
+$('.right-button').click(function() {
+    event.preventDefault();
+    $('.js--section-recommendations__items').animate({
+      scrollLeft: "+=200px"
+    }, "slow");
+ });
+ 
+   $('.left-button').click(function() {
+    event.preventDefault();
+    $('.js--section-recommendations__items').animate({
+      scrollLeft: "-=200px"
+    }, "slow");
+ });
+
+
